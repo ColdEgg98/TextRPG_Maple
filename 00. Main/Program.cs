e485e6afa5da8188b121ec7fa5837123b88c7446
ChinoDaiski <callmeson1@naver.com>
2025-02-11 10:28:44 +0900

    feat: ObjectManager 추가
    - 객체 관리 기능을 담당하는 ObjectManager 구현
    - 오브젝트 생성, 삭제 및 관리 로직 추가

--- 00. Main/Program.cs
+++ 00. Main/Program.cs
@@ -1,6 +1,8 @@
 ﻿using TextRPG_Maple._04._Manager;
 using System.ComponentModel;
 using System.Threading.Channels;
+using System.Runtime.Serialization;
+using TextRPG_Maple._04._Manager._05._Object;
 
 namespace TextRPG_Maple
 {
@@ -12,6 +14,11 @@ namespace TextRPG_Maple
             SoundManager.Instance.LoadSounds();
             SoundManager.Instance.PlaySound(SoundType.BGM, "aLIEz_Piano", true);
             SoundManager.Instance.SetVolume(SoundType.BGM, 0.3f);
+
+            // 플레이어 정보 초기화
+            GameObjectManager.Instance.AddGameObject(ObjectType.PLAYER, "MainPlayer", new Player(""));
+
+            GameObject player = GameObjectManager.Instance.GetGameObject(ObjectType.PLAYER, "MainPlayer");
 
             // 모든 씬을 미리 생성하여 Dictionary에 저장
             var scenes = new Dictionary<SceneType, IScene>
