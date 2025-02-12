0057678bac42f99809c7aad35c486abdfc00c58e
ChinoDaiski <callmeson1@naver.com>
2025-02-12 16:02:46 +0900

    feat: 아이템과 스킬 DB화
    - 아이템과 스킬을 데이터베이스에 저장하고 불러오는 기능 구현

--- 05. Usable/Skill/Skill.cs
+++ 05. Usable/Skill/Skill.cs
@@ -11,7 +11,10 @@ namespace TextRPG_Maple._05._Usable.Skill
 
     internal class Skill : Usable
     {
-        public SkillType SkillType { get; }
+        static List<Skill> skillData = new List<Skill>();
+        static public void SetSkillList(List<Skill> skills) { skillData = skills; }
+
+        public SkillType SkillType { get; set; }
         public int NeedLv { get; set; }
 
         // 테스트용 생성자
@@ -48,22 +51,14 @@ namespace TextRPG_Maple._05._Usable.Skill
 
         public List<Skill> SetWarriorSkill()
         {
-            List<Skill> Warrior_skillSet = new List<Skill>();
-
-            Warrior_skillSet.Add(new Skill("슬래시 블러스트", 3, SkillType.전사, 2, "Mp를 소비하여 주위의 적 다수를 동시에 공격한다.", 10, false));
-            Warrior_skillSet.Add(new Skill("브랜디쉬", 5, SkillType.전사, 2.8f, "눈 앞의 적 여러명을 두 번 연속 공격한다.", 16, false));
-            Warrior_skillSet.Add(new Skill("인사이징", 8, SkillType.전사, 4f, "정신을 집중하여 전방으로 검을 크게 내리긋는다. ", 36, false));
+            List<Skill> Warrior_skillSet = skillData.Where(skill => skill.SkillType == SkillType.전사).ToList();
 
             return Warrior_skillSet;
         }
 
         public List<Skill> SetThief_skillSet()
         {
-            List<Skill> Thief_skillSet = new List<Skill>();
-
-            Thief_skillSet.Add(new Skill("럭키 세븐", 3, SkillType.도적, 1.1f, "행운을 담은 7개의 표창을 던진다.", 14, false));
-            Thief_skillSet.Add(new Skill("트리플 스로우", 6, SkillType.도적, 2.9f, "3개의 표창을 동시에 던진다.", 20, false));
-            Thief_skillSet.Add(new Skill("쇼다운 챌린지", 9, SkillType.도적, 7.8f, "살살 때립니다.", 50, false));
+            List<Skill> Thief_skillSet = skillData.Where(skill => skill.SkillType == SkillType.도적).ToList();
 
             return Thief_skillSet;
         }
